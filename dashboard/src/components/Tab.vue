3055005080dbc410bdb85c06d0f376d9c38522d2
dionisis2014 <toulatosdionisis@gmail.com>
2023-01-03 13:55:41 +0200

    Added basic layout

---
+++ dashboard/src/components/Tab.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="tab">
+		<slot/>
+	</div>
+</template>
+
+<script lang="ts">
+export default {
+	name: 'Tab',
+	props: {
+
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.tab {
+	// Shape
+	@apply w-1/6;
+	@apply overflow-scroll;
+}
+</style>
---
+++ dashboard/src/components/TabItem.vue
@@ -0,0 +1,47 @@
+<template>
+	<button class="tab-item">
+		<slot/>
+	</button>
+</template>
+
+<script lang="ts">
+export default {
+	name: 'TabItem',
+	props: {
+
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.tab-item {
+	// Shape
+	@apply w-full h-12;
+	@apply my-2 px-2 py-1;
+	@apply flex flex-row justify-center items-center;
+	@apply rounded-sm;
+
+	&:first-child {
+		@apply mt-0;
+	}
+
+	&:last-child {
+		@apply mb-0;
+	}
+
+	// Appearance
+	@apply bg-neutral-700;
+	@apply cursor-pointer;
+	@apply shadow-md;
+	@apply font-mono;
+	@apply text-lg font-bold;
+
+	&:hover {
+		@apply bg-neutral-500;
+	}
+
+	&:active {
+		@apply bg-neutral-600;
+	}
+}
+</style>
